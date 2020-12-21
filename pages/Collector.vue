139b707f1d804fadf6abe7ffddd46d6cccf6a3d7
Ugur Ertas <ugurertas9@gmail.com>
2020-12-21 23:17:51 +0100

    collectors page overview works

--- pages/Collector.vue
+++ pages/Collector.vue
@@ -19,32 +19,19 @@
       <nb-item :style="{ borderColor: '#62B1F6' }">
         <nb-input placeholder="zoek schuldeiser documenten" />
       </nb-item>
-      <nb-list>
-        <nb-list-item>
+      <nb-list v-if="dataIsReady">
+        <nb-list-item v-for="collector in clientCollectors" :key="collector.ID">
           <nb-left>
-            <nb-text>aanmaning ziggo</nb-text>
+            <nb-text class="text">{{collector.client_debt.Incasseerder}}</nb-text>
           </nb-left>
           <nb-body>
-            <nb-text>24-09-2020</nb-text>
-          </nb-body>
-        </nb-list-item>
-        <nb-list-item>
-          <nb-left>
-            <nb-text>aanmaning ziggo</nb-text>
-          </nb-left>
-          <nb-body>
-            <nb-text>24-09-2020</nb-text>
-          </nb-body>
-        </nb-list-item>
-        <nb-list-item>
-          <nb-left>
-            <nb-text>aanmaning ziggo</nb-text>
-          </nb-left>
-          <nb-body>
-            <nb-text>24-09-2020</nb-text>
+            <nb-text class="text">{{collector.DateTime}}</nb-text>
           </nb-body>
         </nb-list-item>
       </nb-list>
+      <nb-card-item class="loadingWrapper" v-else>
+			  <image :source="require('../assets/images/loader.gif')" class="loading" />
+	   </nb-card-item>
     </nb-content>
     <nb-footer>
       <footer-nav
@@ -54,22 +41,90 @@
     </nb-footer>
   </nb-container>
 </template>
+<style>
+.headerText {
+  color: white;
+  font-weight: bold;
+}
+.detailText {
+  color: white;
+}
+.marginBottom {
+  margin-bottom: 20px;
+}
+.text {
+  color: #0078ae;
+}
 
+.loadingWrapper {
+  align-items: center;
+  justify-content: center;
+  flex:1;
+}
+
+.loading {
+  height:50;
+  width:50;
+}
+</style>
 <script>
 import FooterNav from '../included/Footer';
+import { AsyncStorage } from 'react-native';
+
 export default {
   props: {
     navigation: {
       type: Object,
+       user: {},
     },
+  },
+    created() {
+    this.userData();
   },
   components: { FooterNav },
   data() {
     return {
       selectedDoc: '0',
+       clientCollectors: {},
+       dataIsReady: false
     };
   },
   methods: {
+        userData: async function () {
+      let value = '';
+      try {
+        value = await AsyncStorage.getItem('login');
+        this.user = JSON.parse(value);
+      } catch (error) {
+        // Error retrieving data
+        console.log(error.message);
+      }
+
+      try {
+        let response = await fetch('http://api.arsus.nl/client/docs/debtors', {
+          method: 'POST',
+          headers: {
+            Accespt: 'application/json',
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({
+            email: this.user.email,
+            password: this.user.password,
+          }),
+        });
+
+        let responseJson = await response.json();
+        if (responseJson.success) {
+          this.clientCollectors = responseJson.results;
+          this.dataIsReady = true;
+        } else {
+          console.log(responseJson);
+        }
+      } catch (error) {
+        console.log(error);
+        console.error(error);
+      }
+    },
     goBack: function () {
       this.navigation.goBack();
     },
