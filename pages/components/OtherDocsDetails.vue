fe2e81b18a390ab4f753314eda869dd4946dbe9f
Ugur22 <ugurertas9@gmail.com>
2021-02-05 22:40:16 +0100

    fix images otherdoc not showing

--- pages/components/OtherDocsDetails.vue
+++ pages/components/OtherDocsDetails.vue
@@ -33,11 +33,10 @@ export default {
   created() {
 	},
 	mounted() {
-		fetchData(`document/file-download?client_id=${this.navigation.getParam('ClientID')}
-				&document_id=${this.navigation.getParam('docID')}`,this.$root.user.token,'file').then(val => {
+		fetchData(`document/file-download?client_id=${this.navigation.getParam('ClientID')}&document_id=${this.navigation.getParam('docID')}`,this.$root.user.token,'file').then(val => {
 			let that = this;
 			this.dataIsReady = true;
-			that.otherDoc = val;
+      that.otherDoc = val;
 			});
 	},
   methods: {
